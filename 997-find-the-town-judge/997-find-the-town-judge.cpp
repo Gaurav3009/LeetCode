536446c469a2feb0ea050614daa22b2cf9ccd739
Gaurav Pandey <57361998+Gaurav3009@users.noreply.github.com>
2022-04-16 10:27:39 +0530

    Time: 148 ms (98.05%), Space: 60.8 MB (81.35%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -6,24 +6,37 @@ public:
         }else if(trust.size() == 0 && n != 1){
             return -1;
         }
-        map<int, int> outdegree;
-        map<int, int> indegree;
-        for(int i = 0; i < trust.size(); i++){
-            if(outdegree[trust[i][0]] == -1){
-                outdegree[trust[i][0]] = 0;
-            }
-            outdegree[trust[i][0]]++;
-            indegree[trust[i][1]]++;
-            if(outdegree[trust[i][1]] == 0){
-                outdegree[trust[i][1]] = -1;
-            }
+        // map<int, int> outdegree;
+        // map<int, int> indegree;
+        // for(int i = 0; i < trust.size(); i++){
+        //     if(outdegree[trust[i][0]] == -1){
+        //         outdegree[trust[i][0]] = 0;
+        //     }
+        //     outdegree[trust[i][0]]++;
+        //     indegree[trust[i][1]]++;
+        //     if(outdegree[trust[i][1]] == 0){
+        //         outdegree[trust[i][1]] = -1;
+        //     }
+        // }
+        // for(auto it : outdegree){
+        //     cout<<it.first<<" "<<it.second<<endl;
+        //     if(it.second == -1 && indegree[it.first] == outdegree.size() - 1){
+        //         return it.first;
+        //     }
+        // }
+        // return -1;
+        vector<int> out(n + 1, 0);
+        vector<int> in(n + 1, 0);
+        for(int i = 0; i <trust.size(); i++){
+            out[trust[i][0]]++;
+            in[trust[i][1]]++;
         }
-        for(auto it : outdegree){
-            cout<<it.first<<" "<<it.second<<endl;
-            if(it.second == -1 && indegree[it.first] == outdegree.size() - 1){
-                return it.first;
+        for(int i = 0; i < n  +1; i++){
+            if(in[i] == n - 1 && out[i] == 0){
+                return i;
             }
         }
         return -1;
+        
     }
 };

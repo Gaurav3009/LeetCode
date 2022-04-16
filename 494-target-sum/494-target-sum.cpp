bc9f44ab96d514114b6eee5c2674a2bdb327e61e
Gaurav Pandey <57361998+Gaurav3009@users.noreply.github.com>
2022-04-16 19:19:55 +0530

    Time: 10 ms (83.53%), Space: 9.2 MB (60.33%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,67 +1,36 @@
 class Solution {
 public:
-    // int countSubsetSum(vector<int> arr, int n, int sum){
-    //     int dp[n + 1][sum + 1];
-    //     for(int i = 0; i < sum + 1; i++){
-    //         dp[0][i] = 0;
-    //     }
-    //     for(int i = 0; i < n + 1; i++){
-    //         dp[i][0] = 1;
-    //     }
-    //     for(int i = 1; i < n + 1; i++){
-    //         for(int j = 1; j < sum + 1; j++){
-    //             if(arr[i - 1] <= j){
-    //                 dp[i][j] = dp[i - 1][j - arr[i - 1]] + dp[i - 1][j];
-    //             }else{
-    //                 dp[i][j] = dp[i - 1][j];
-    //             }
-    //         }
-    //     }
-    //     return dp[n][sum];
-    // }
-    // int findTargetSumWays(vector<int>& nums, int target) {
-    //     target = abs(target);
-    //     int sum = 0;
-    //     int n = nums.size();
-    //     for(int i = 0; i < n; i++){
-    //         sum += nums[i];
-    //     }
-    //     if(target > sum || (sum + target) % 2 != 0){
-    //         return 0;
-    //     }
-    //     sum = (target + sum) / 2;
-    //     return countSubsetSum(nums, n, sum);
-    // }
-    int dynamic_programming(vector<int> &nums, int n, int sum){
-        int dp[n+1][sum+1];
-        
-        for(int i = 0; i<n+1; i++) dp[i][0] = 1;
-        for(int i = 1; i<sum+1; i++) dp[0][i] = 0;
-        
-        for(int i = 1; i<n+1; i++){
-            for(int j = 0; j<sum+1; j++){
-                if(nums[i-1] <= j){
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i-1]];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
+    int countSubsetSum(vector<int> arr, int n, int sum){
+        int dp[n + 1][sum + 1];
+        for(int i = 0; i < sum + 1; i++){
+            dp[0][i] = 0;
+        }
+        for(int i = 0; i < n + 1; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 0; j < sum + 1; j++){
+                if(arr[i - 1] <= j){
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] + dp[i - 1][j];
+                }else{
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
         return dp[n][sum];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        target=abs(target);
-        int n = nums.size();
+        target = abs(target);
         int sum = 0;
-        
-        for(int i = 0; i<n; i++) sum += nums[i];
-        
-         if(target > sum || (sum+target)%2!=0)
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+        }
+        if(target > sum || (sum + target) % 2 != 0){
             return 0;
-        
-        int diff = (target + sum)/2;
-        
-        return dynamic_programming(nums, n , diff);
+        }
+        sum = (target + sum) / 2;
+        return countSubsetSum(nums, n, sum);
     }
+    
 };

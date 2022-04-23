aeaa0a458e95d8ab2e6b78efef3076fed94c4293
Gaurav Pandey <57361998+Gaurav3009@users.noreply.github.com>
2022-04-23 19:58:02 +0530

    Time: 20 ms (9.37%), Space: 5.7 MB (95.59%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -12,5 +12,24 @@ public:
             return count - 1;
         }
         return count;
+        
+    //     long arr[n];
+    //     arr[0] = 1;
+    //     for(int i = 1; i < n; i++){
+    //         arr[i] += arr[i - 1] + (i + 1); 
+    //     }
+    //     int start = 0, mid, end = n - 1, res;
+    //     while(start < end){
+    //         mid = start + (end - start) / 2;
+    //         if(arr[mid] == n){
+    //             return mid;
+    //         }else if(arr[mid] < n){
+    //             start = mid + 1;
+    //         }else if(arr[mid] > n){
+    //             res = mid;
+    //             end = mid - 1;
+    //         }
+    //     }
+    //     return res;
     }
 };

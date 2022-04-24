5e3b0d57b26eeb4f6a42fbcaf44d5dd2e488e2b0
Gaurav Pandey <57361998+Gaurav3009@users.noreply.github.com>
2022-04-24 09:35:36 +0530

    Time: 181 ms (67.49%), Space: 57.2 MB (96.16%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,9 +1,7 @@
 class UndergroundSystem {
 public:
     map<double,pair<string,double>> customer;
-	//customer is storing customer id as key and stationname and time as value
     map<pair<string, string>, pair<double, double>> total;
-	//total is storing startstation and endstation as key and totaltime and count as value
     UndergroundSystem() {
         customer.clear();
         total.clear();
